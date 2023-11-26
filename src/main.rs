7bd6d02f787bfa6bae7e8237c228646c56c5e437
kdnakt <a.kid.1985@gmail.com>
2023-11-26 14:35:20 +0900

    first implementation of grrs

--- src/main.rs
+++ src/main.rs
@@ -17,5 +17,12 @@ fn main() {
 
     let args = Cli::parse();
 
-    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
+    let content = std::fs::read_to_string(&args.path)
+        .expect("could not read file");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{line}");
+        }
+    }
 }

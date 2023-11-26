a9f76f8ec74c63e8b95cb77851ea3b6b0d43adeb
kdnakt <a.kid.1985@gmail.com>
2023-11-26 14:45:42 +0900

    use a BufReader instead of read_to_string()

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::io::BufRead;
 
 #[derive(Parser)]
 struct Cli {
@@ -6,7 +7,7 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<(), std::io::Error> {
     // let pattern = std::env::args().nth(1).expect("no pattern given");
     // let path = std::env::args().nth(2).expect("no path given");
 
@@ -17,12 +18,17 @@ fn main() {
 
     let args = Cli::parse();
 
-    let content = std::fs::read_to_string(&args.path)
-        .expect("could not read file");
+    // let content = std::fs::read_to_string(&args.path)
+    //     .expect("could not read file");
 
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{line}");
+    let f = std::fs::File::open(args.path)?;
+    let reader = std::io::BufReader::new(f);
+
+    for line in reader.lines() {
+        if line.as_ref().expect("not a line").contains(&args.pattern) {
+            println!("{}", line?);
         }
     }
+
+    Ok(())
 }
